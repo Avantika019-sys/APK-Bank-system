513bda4198f2363bee2df4385a210c06d1fa6f00
Ali <alinajafi2104@yahoo.com>
2025-11-26 12:29:16 +0100

    main

--- main.cpp
+++ main.cpp
@@ -2,22 +2,28 @@
 #include <iostream>
 #include <ostream>
 
+#include "account.h"
+#include "bank.h"
 #include "stock/server.h"
 
 int main() {
   auto &server = stock::server::getInstance();
   std::thread stockUpdaterThread([&]() { server.startUpdateStocksWorker(); });
   std::thread stockOrderThread([&]() { server.startStockWorker(); });
-
+  Account acc;
+  Bank danskeBank("Danske bank");
+  Bank nordea("Nordea");
+  Bank jyskeBank("Jyske Bank");
   bool run = true;
+
   while (run) {
     int input;
     std::cin >> input;
     std::cout << "1: withdraw money " << std::endl;
     std::cout << "2: deposit money " << std::endl;
-    std::cout << "3: " << std::endl;
-    std::cout << "4: " << std::endl;
-    std::cout << "5: " << std::endl;
+    std::cout << "3: buy stock" << std::endl;
+    std::cout << "4: sell stock" << std::endl;
+    std::cout << "5: switch bank" << std::endl;
     std::cout << "6: " << std::endl;
     std::cout << "7: " << std::endl;
     std::cout << "8: " << std::endl;
@@ -25,30 +31,49 @@ int main() {
     std::cout << "e: exit program" << std::endl;
 
     switch (input) {
-      case '1':
-        std::cout << "1" << std::endl;
-      case '2':
-        std::cout << "2" << std::endl;
-      case '3':
-        std::cout << "3" << std::endl;
-      case '4':
-        std::cout << "4" << std::endl;
-      case '5':
-        std::cout << "5" << std::endl;
-      case '6':
-        std::cout << "6" << std::endl;
-      case '7':
-        std::cout << "7" << std::endl;
-      case '8':
-        std::cout << "8" << std::endl;
-      case '9':
-        std::cout << "9" << std::endl;
-      case 'e': {
-        server.stopWorkers();
-        run = false;
+    case '1':
+      std::cout << "Enter amount to withdraw" << std::endl;
+      int withdrawAmount;
+      std::cin >> withdrawAmount;
+      acc.withdraw(withdrawAmount);
+
+    case '2':
+      std::cout << "Enter amount to deposit" << std::endl;
+      int depositAmount;
+      std::cin >> depositAmount;
+      acc.withdraw(depositAmount);
+
+    case '3':
+      std::cout << "Enter number of bank to switch to" << std::endl;
+      std::cout << "Available banks:" << std::endl;
+      std::cout << "1 : Dansk Bank" << std::endl;
+      std::cout << "2 : Nordea" << std::endl;
+      std::cout << "3 : JyskeBank" << std::endl;
+      int bankNo;
+      std::cin >> bankNo;
+      switch (bankNo) {
+      case 1:
+      case 2:
+      case 3:
       }
-      default:
-        std::cout << "Unknown input" << std::endl;
+    case '4':
+      std::cout << "4" << std::endl;
+    case '5':
+      std::cout << "5" << std::endl;
+    case '6':
+      std::cout << "6" << std::endl;
+    case '7':
+      std::cout << "7" << std::endl;
+    case '8':
+      std::cout << "8" << std::endl;
+    case '9':
+      std::cout << "9" << std::endl;
+    case 'e': {
+      server.stopWorkers();
+      run = false;
+    }
+    default:
+      std::cout << "Unknown input" << std::endl;
     }
   }
   stockUpdaterThread.join();
