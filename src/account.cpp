b744aeb122906e4f634ecd8fb24bad976eebcb40
Avantika019-sys <202207320@post.au.dk>
2025-12-01 15:57:30 +0100

    commit

--- src/account.cpp
+++ src/account.cpp
@@ -10,7 +10,9 @@ Account::Account(string holderName, string type, double initialBalance)
     } else {
         throw std::runtime_error("Maximum number of accounts reached.");
     }
+    log.addEntry("Account created for " + holderName + " with initial balance " + to_string(initialBalance));
 }
+
 Account::~Account() {
     accountCount--;
 }
@@ -22,9 +24,47 @@ int Account::getAccountCount() {
 double Account::getBalance() const {
     return balance;
 }
+
 string Account::getAccountType() const {
     return accountType;
 }
+
 string Account::getAccountHolderName() const {
     return accountHolderName;
 }
+
+void Account::addAccount(unique_ptr<Account> account) {
+    if (canCreateAccount()) {
+        accounts.push_back(std::move(account));
+    } else {
+        throw std::runtime_error("Cannot create more accounts. Maximum limit reached.");
+    }
+}
+
+vector<Account*> Account::getAllAccounts() {
+    vector<Account*> accountPointers;
+    for (const auto& acc : accounts) {
+        accountPointers.push_back(acc.get());
+    }
+    return accountPointers;
+}
+
+void Account::deposit(double amount) {
+    if (amount > 0) {
+        balance += amount;
+    } else {
+        throw std::runtime_error("Deposit amount must be positive.");
+    }
+}
+
+void Account::withdraw(double amount) {
+    if (amount > 0 && amount <= balance) {
+        balance -= amount;
+    } else {
+        throw std::runtime_error("Invalid withdrawal amount.");
+    }
+}
+
+//concopts used:
+// - runtime_error for exception handling
+// - basic garuntees for constructor
